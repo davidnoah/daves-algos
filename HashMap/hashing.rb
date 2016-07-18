50c694d373b49650a2f21aec5c1854e9ab3cd22f
davidnoah <david.t.noah@gmail.com>
2016-07-18 14:49:52 -0700

    refactor files

--- HashMap/hashing.rb
+++ HashMap/hashing.rb
@@ -1,7 +1,3 @@
-class Fixnum
-  # Fixnum#hash already implemented for you
-end
-
 class Array
   def hash
     each_with_index.inject(0) do |intermediate_hash, (el, i)|
@@ -18,8 +14,6 @@ class String
 end
 
 class Hash
-  # This returns 0 because rspec will break if it returns nil
-  # Make sure to implement an actual Hash#hash method
   def hash
     to_a.sort_by(&:hash).hash
   end
